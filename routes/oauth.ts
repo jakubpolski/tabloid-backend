68b50cb611277e252ece8a66f946b0e161d82873
Jakub Polski <jakubpolski@gmail.com>
2026-01-30 15:59:38 +0100

    fixes

--- routes/oauth.ts
+++ routes/oauth.ts
@@ -75,8 +75,8 @@ router.get('/', async (req: Request, res: Response) => {
 router.get('/logout', (req: Request, res: Response) => {
     res.clearCookie('token', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        secure: true,
+        sameSite: 'none',
     });
     res.json({ message: 'Logged out successfully' });
 });
